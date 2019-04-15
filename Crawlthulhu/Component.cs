2eb4ed2cc722c803d2efd606195a2866e416fd62
Morten Bay Skov <mort79e5@student.eadania.dk>
2019-04-15 13:57:37 +0200

    dadada

---
+++ Crawlthulhu/Component.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crawlthulhu
+{
+    public abstract class Component
+    {
+        private GameObject gameObject;
+
+        public GameObject GameObject
+        {
+            get
+            {
+                return gameObject;
+            }
+            set
+            {
+                this.gameObject = value;
+            }
+        } 
+
+        /// <summary>
+        /// Attaches the Components to current GameObject
+        /// </summary>
+        /// <param name="gameObject"></param>
+        public virtual void Attach(GameObject gameObject)
+        {
+            this.gameObject = gameObject;
+        }
+
+        /// <summary>
+        /// Loads the content of current Component, to the game 
+        /// </summary>
+        /// <param name="content"></param>
+        public virtual void LoadContent(ContentManager content)
+        {
+
+        }
+
+        /// <summary>
+        /// Updates current Component's game logic
+        /// </summary>
+        /// <param name="gameTime"></param>
+        public virtual void Update(GameTime gameTime)
+        {
+
+        }
+
+        /// <summary>
+        /// Used for drawing out current component to the screen
+        /// </summary>
+        /// <param name="spriteBatch"></param>
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+
+    }
+}
---
+++ Crawlthulhu/GameObject.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crawlthulhu
+{
+    public class GameObject
+    {
+        private List<Component> components = new List<Component>();
+
+
+        /// <summary>
+        /// GameObject's Constructor
+        /// </summary>
+        public GameObject()
+        {
+
+        }
+
+        public void LoadContent(ContentManager content)
+        {
+            foreach(Component component in components)
+            {
+                component.LoadContent(content);
+            }
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            foreach(Component component in components)
+            {
+                component.Update(gameTime);
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            foreach(Component component in components)
+            {
+                component.Draw(spriteBatch);
+            }
+        }
+
+        public void AddComponent(Component component)
+        {
+            component.Attach(this);
+            components.Add(component);
+        }
+
+        public Component GetComponent(string component)
+        {
+            return components.Find(x => x.GetType().Name == component);
+        }
+
+        public void RemoveComponent(Component component)
+        {
+            components.Remove(component);
+        }
+
+    }
+}
